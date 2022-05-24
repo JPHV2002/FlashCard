2af78e9dff3f3b33010f6a22fc80ab98f108d456
Eric de Lira Candido <ericdelira2001@gmail.com>
2022-05-24 10:19:07 -0300

    Intalação do axios

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-
 import Routes from './routes'
 
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/backend/', Routes)
